9c979bb6446f797a397f947b50bfb6f6b75e456a
bishal <bishalpd@gmail.com>
2023-08-27 17:09:59 +0600

    initial commit

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavUI from "./NavUI";
+
+const Navbar = () => {
+  return (
+    <div>
+      <NavUI cartItemsCount={0} />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/hooks/useAuth.ts
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Auth {
+  loading: boolean;
+  loggedIn: boolean;
+  isAdmin: boolean;
+}
+
+const useAuth = (): Auth => {
+  return {
+    loading: false,
+    loggedIn: false,
+    isAdmin: false,
+  };
+};
+
+export default useAuth;
